d234469b384e1e251d6dc14a2aa57cf26690638b
nakaG <nakaG@NAKAJ_NOTE>
2009-05-21 05:58:53 +0900

    再帰表とリレーションシップをはれるように修正

--- tmdmaker/src/jp/sourceforge/tmdmaker/editpart/RecursiveTableEditPart.java
+++ tmdmaker/src/jp/sourceforge/tmdmaker/editpart/RecursiveTableEditPart.java
@@ -3,6 +3,7 @@ package jp.sourceforge.tmdmaker.editpart;
 import java.util.List;
 import java.util.Map;
 
+import jp.sourceforge.tmdmaker.editpolicy.AbstractEntityGraphicalNodeEditPolicy;
 import jp.sourceforge.tmdmaker.figure.EntityFigure;
 import jp.sourceforge.tmdmaker.model.AbstractEntityModel;
 import jp.sourceforge.tmdmaker.model.Attribute;
@@ -21,10 +22,16 @@ import org.eclipse.gef.requests.GroupRequest;
 /**
  * 
  * @author nakaG
- *
+ * 
  */
 public class RecursiveTableEditPart extends AbstractEntityEditPart {
 
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.gef.editparts.AbstractGraphicalEditPart#createFigure()
+	 */
 	@Override
 	protected IFigure createFigure() {
 		EntityFigure figure = new EntityFigure();
@@ -33,17 +40,24 @@ public class RecursiveTableEditPart extends AbstractEntityEditPart {
 		return figure;
 	}
 
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see jp.sourceforge.tmdmaker.editpart.AbstractEntityEditPart#updateFigure(org.eclipse.draw2d.IFigure)
+	 */
 	@Override
 	protected void updateFigure(IFigure figure) {
 		EntityFigure entityFigure = (EntityFigure) figure;
-		RecursiveTable table = (RecursiveTable)getModel();
-//		List<Identifier> ids = table.getReuseKeys();
+		RecursiveTable table = (RecursiveTable) getModel();
+		// List<Identifier> ids = table.getReuseKeys();
 		List<Attribute> atts = table.getAttributes();
 		entityFigure.removeAllRelationship();
 		entityFigure.removeAllAttributes();
 
 		entityFigure.setEntityName(table.getName());
-		for (Map.Entry<AbstractEntityModel, ReuseKey> rk : table.getReuseKeys().entrySet()) {
+		for (Map.Entry<AbstractEntityModel, ReuseKey> rk : table.getReuseKeys()
+				.entrySet()) {
 			for (Identifier i : rk.getValue().getIdentifires()) {
 				entityFigure.addRelationship(i.getName());
 			}
@@ -53,28 +67,63 @@ public class RecursiveTableEditPart extends AbstractEntityEditPart {
 		}
 	}
 
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see org.eclipse.gef.editparts.AbstractEditPart#createEditPolicies()
+	 */
 	@Override
 	protected void createEditPolicies() {
-		installEditPolicy(EditPolicy.COMPONENT_ROLE, new RecursiveTableComponentEditPolicy());
-
+		installEditPolicy(EditPolicy.COMPONENT_ROLE,
+				new RecursiveTableComponentEditPolicy());
+		installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE,
+				new AbstractEntityGraphicalNodeEditPolicy());
 	}
-	private class RecursiveTableComponentEditPolicy extends ComponentEditPolicy {
 
+	/**
+	 * 
+	 * @author nakaG
+	 * 
+	 */
+	private static class RecursiveTableComponentEditPolicy extends
+			ComponentEditPolicy {
+
+		/**
+		 * 
+		 * {@inheritDoc}
+		 * 
+		 * @see org.eclipse.gef.editpolicies.ComponentEditPolicy#createDeleteCommand(org.eclipse.gef.requests.GroupRequest)
+		 */
 		@Override
 		protected Command createDeleteCommand(GroupRequest deleteRequest) {
-			RecursiveTableDeleteCommand command = 
-				new RecursiveTableDeleteCommand(
-						(Diagram) getHost().getParent().getModel(),
-						(RecursiveTable) getHost().getModel());
+			RecursiveTableDeleteCommand command = new RecursiveTableDeleteCommand(
+					(Diagram) getHost().getParent().getModel(),
+					(RecursiveTable) getHost().getModel());
 			return command;
 		}
-		
+
 	}
-	private static class RecursiveTableDeleteCommand extends ConnectableElementDeleteCommand {
+
+	/**
+	 * 
+	 * @author nakaG
+	 * 
+	 */
+	private static class RecursiveTableDeleteCommand extends
+			ConnectableElementDeleteCommand {
+		/** 再帰表 */
 		private RecursiveTable model;
 
-		public RecursiveTableDeleteCommand(Diagram diagram,
-				RecursiveTable model) {
+		/**
+		 * コンストラクタ
+		 * 
+		 * @param diagram
+		 *            親
+		 * @param model
+		 *            再帰表
+		 */
+		public RecursiveTableDeleteCommand(Diagram diagram, RecursiveTable model) {
 			super();
 			this.diagram = diagram;
 			this.model = model;
@@ -82,6 +131,12 @@ public class RecursiveTableEditPart extends AbstractEntityEditPart {
 			this.targetConnections.addAll(model.getModelTargetConnections());
 		}
 
+		/**
+		 * 
+		 * {@inheritDoc}
+		 * 
+		 * @see org.eclipse.gef.commands.Command#execute()
+		 */
 		@Override
 		public void execute() {
 
@@ -91,6 +146,12 @@ public class RecursiveTableEditPart extends AbstractEntityEditPart {
 			detachConnections(targetConnections);
 		}
 
+		/**
+		 * 
+		 * {@inheritDoc}
+		 * 
+		 * @see org.eclipse.gef.commands.Command#undo()
+		 */
 		@Override
 		public void undo() {
 			diagram.addChild(model);
--- tmdmaker/src/jp/sourceforge/tmdmaker/model/RecursiveRelationship.java
+++ tmdmaker/src/jp/sourceforge/tmdmaker/model/RecursiveRelationship.java
@@ -2,59 +2,82 @@ package jp.sourceforge.tmdmaker.model;
 
 import org.eclipse.draw2d.geometry.Rectangle;
 
+/**
+ * 
+ * @author nakaG
+ *
+ */
 public class RecursiveRelationship extends Relationship {
-//	private RecursiveMarkConnection connection;
+	/** 再帰表 */
 	private RecursiveTable table;
+	/** 親 */
+	private Diagram diagram;
 
+	/**
+	 * コンストラクタ
+	 * @param source 再帰元エンティティ
+	 */
 	public RecursiveRelationship(AbstractEntityModel source) {
 		this.source = source;
-//		this.connection = new RecursiveMarkConnection();
+		// this.connection = new RecursiveMarkConnection();
 		this.table = new RecursiveTable();
 		Rectangle constraint = source.getConstraint().getTranslated(100, 0);
 		this.table.setConstraint(constraint);
 		this.target = table;
-//		this.connection.setSource(this);
-//		this.connection.setTarget(source);
+		this.diagram = this.source.getDiagram();
+		// this.connection.setSource(this);
+		// this.connection.setTarget(source);
 		// 再帰表の類別は元エンティティを引き継ぐ
 		table.setEntityType(source.getEntityType());
 		table.setConstraint(source.getConstraint().getTranslated(100, 0));
 		table.setName(source.getName() + "." + source.getName() + "." + "再帰表");
 
 	}
-	
-	/* (non-Javadoc)
-	 * @see tm.tmdiagram.tmdeditor.model.AbstractConnectionModel#setTarget(tm.tmdiagram.tmdeditor.model.ConnectableElement)
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 * 
+	 * @see
+	 * tm.tmdiagram.tmdeditor.model.AbstractConnectionModel#setTarget(tm.tmdiagram
+	 * .tmdeditor.model.ConnectableElement)
 	 */
 	@Override
 	public void setTarget(AbstractEntityModel target) {
 		// source == targetのため設定しない
-//		super.setTarget(target);
+		// super.setTarget(target);
 	}
 
-	/* (non-Javadoc)
+	/**
+	 * {@inheritDoc}
+	 *  
 	 * @see tm.tmdiagram.tmdeditor.model.AbstractConnectionModel#connect()
 	 */
 	@Override
 	public void connect() {
-		((AbstractEntityModel)this.source).getDiagram().addChild(table);
-		if (!source.getModelSourceConnections().contains(this)){
+		diagram.addChild(table);
+		table.setDiagram(diagram);
+		if (!source.getModelSourceConnections().contains(this)) {
 			System.out.println("source=" + source.getClass().toString());
 			source.addSourceConnection(this);
 		}
 		table.addTargetConnection(this);
 		attachSource();
 		attachTarget();
-		table.addReuseKey((AbstractEntityModel)source);
-		
+		table.addReuseKey((AbstractEntityModel) source);
+
 	}
 
-	/* (non-Javadoc)
-	 * @see tm.tmdiagram.tmdeditor.model.AbstractConnectionModel#detachSource()
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see jp.sourceforge.tmdmaker.model.AbstractConnectionModel#disConnect()
 	 */
 	@Override
-	public void detachSource() {
+	public void disConnect() {
 		source.removeSourceConnection(this);
 		table.removeTargetConnection(this);
-		((AbstractEntityModel)this.source).getDiagram().removeChild(table);
+		table.setDiagram(null);
+		diagram.removeChild(table);
 	}
 }
