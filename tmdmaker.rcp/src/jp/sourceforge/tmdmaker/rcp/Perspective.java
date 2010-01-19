60f64f50a889f18bd06e0d89cd78838d1f09733c
nakag <nakag@users.sourceforge.jp>
2010-01-20 05:56:46 +0900

    Initial commit

---
+++ tmdmaker.rcp/src/jp/sourceforge/tmdmaker/rcp/Perspective.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2009 TMD-Maker Project <http://tmdmaker.sourceforge.jp/>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.sourceforge.tmdmaker.rcp;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+/**
+ * 
+ * @author nakaG
+ *
+ */
+public class Perspective implements IPerspectiveFactory {
+
+	/**
+	 * 
+	 * {@inheritDoc}
+	 *
+	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		String editorArea = layout.getEditorArea();
+		layout.setEditorAreaVisible(true);
+
+		layout.setFixed(false);
+		layout.addView("org.eclipse.ui.navigator.ProjectExplorer", IPageLayout.LEFT, 0.3f, editorArea);
+		layout.addView("org.eclipse.ui.views.ContentOutline", IPageLayout.RIGHT, 1.0f, editorArea);
+	}
+}
