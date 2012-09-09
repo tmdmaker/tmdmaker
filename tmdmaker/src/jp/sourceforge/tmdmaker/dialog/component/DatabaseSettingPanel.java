86e8a1953be016b05d3628ba3cabbbb077c8abc0
nakag <nakag@users.sourceforge.jp>
2012-09-09 15:24:34 +0900

    #29351 対応
    ダイアログの表示部品のサイズ調整。

--- tmdmaker/src/jp/sourceforge/tmdmaker/dialog/component/DatabaseSettingPanel.java
+++ tmdmaker/src/jp/sourceforge/tmdmaker/dialog/component/DatabaseSettingPanel.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010 TMD-Maker Project <http://tmdmaker.sourceforge.jp/>
+ * Copyright 2009-2012 TMD-Maker Project <http://tmdmaker.sourceforge.jp/>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -46,7 +46,7 @@ public class DatabaseSettingPanel extends Composite {
 		databaseNameLabel.setText("データベース名");
 		this.setLayout(gridLayout);
 		createDatabaseCombo();
-		setSize(new Point(217, 30));
+		setSize(new Point(217, 40));
 	}
 
 	/**
