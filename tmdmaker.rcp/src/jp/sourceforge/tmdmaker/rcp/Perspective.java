12f3699bde73cfed0b03c2e86fa4e9029696f5f8
nakag <nakag@users.sourceforge.jp>
2012-09-15 16:50:46 +0900

    ビューを閉じられないように設定変更。

--- tmdmaker.rcp/src/jp/sourceforge/tmdmaker/rcp/Perspective.java
+++ tmdmaker.rcp/src/jp/sourceforge/tmdmaker/rcp/Perspective.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 TMD-Maker Project <http://tmdmaker.sourceforge.jp/>
+ * Copyright 2009-2012 TMD-Maker Project <http://tmdmaker.sourceforge.jp/>
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,12 +33,6 @@ public class Perspective implements IPerspectiveFactory {
 	 * @see org.eclipse.ui.IPerspectiveFactory#createInitialLayout(org.eclipse.ui.IPageLayout)
 	 */
 	public void createInitialLayout(IPageLayout layout) {
-		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(true);
-
-		layout.addView(IPageLayout.ID_PROJECT_EXPLORER, IPageLayout.LEFT, 0.3f,
-				editorArea);
-		layout.addView(IPageLayout.ID_OUTLINE, IPageLayout.RIGHT, 1.0f,
-				editorArea);
 	}
 }
