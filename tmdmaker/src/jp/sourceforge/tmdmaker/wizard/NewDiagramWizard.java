4a642a770d6e3ea71c14b2824d05ed2a4f1a5c54
nakag <nakag@users.sourceforge.jp>
2017-02-11 11:13:21 +0900

    Add intro page.
    初期版。多言語対応等のリファクタリングを後で行う。

--- tmdmaker/src/jp/sourceforge/tmdmaker/wizard/NewDiagramWizard.java
+++ tmdmaker/src/jp/sourceforge/tmdmaker/wizard/NewDiagramWizard.java
@@ -110,6 +110,8 @@ public class NewDiagramWizard extends Wizard implements INewWizard {
 			super(string, selection);
 			setFileExtension("tmd"); //$NON-NLS-1$
 			setFileName("diagram.tmd"); //$NON-NLS-1$
+			setTitle("TMダイアグラム");
+			setDescription("新規のTMダイアグラムを作成します。");
 		}
 
 		/**
