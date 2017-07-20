ed68f99c8e14d06e6a3736ee31e549f4b477ba75
tohosaku <ny@cosmichorror.org>
2017-07-20 22:18:05 +0900

    swith from docbook to sphinx

---
+++ tmdmaker.doc/src/site/sphinx/conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# sphinx-maven-plugin と python3 の両方で動かすため
+import sys
+if sys.version_info[0] == 2:
+   sys.setdefaultencoding("utf-8")
+
+extensions = ['sphinx.ext.todo']
+
+templates_path = ['_templates']
+
+source_suffix = ['.rst', '.md']
+
+source_encoding = 'utf-8-sig'
+
+master_doc = 'index'
+
+project = 'TMD-Maker'
+copyright = '2017, TMD-Maker Project'
+author = 'TMD-Maker Project'
+version = '0.5'
+release = '0.5-SNAPSHOT'
+
+language = 'ja'
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+pygments_style = 'sphinx'
+todo_include_todos = True
+
+html_theme = 'bizstyle'
+html_static_path = ['_static']
---
+++ tmdmaker.doc/src/site/sphinx/index.rst
@@ -0,0 +1,28 @@
+.. TMD-Maker documentation master file, created by
+   sphinx-quickstart on Tue Jul 18 19:11:12 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+TMD-Maker ドキュメント
+=======================
+
+このドキュメントでは、TMD-Makerの説明とTMD-Makerを使ったTMD(T字形ER図)の作成方法を解説します。
+
+※内容はTMD-Makerの進化と、私のTM(T字形ER手法)の理解の深化に伴い順次更新します。
+
+目次:
+------
+
+.. toctree::
+   :maxdepth: 2
+
+   p1
+   p2
+   p3
+
+索引と検索
+-----------
+
+* :ref:`genindex`
+* :ref:`search`
+
