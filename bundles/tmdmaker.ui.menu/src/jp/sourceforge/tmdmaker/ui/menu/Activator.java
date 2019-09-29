7ad49165c9a0c8f08890563b7294a96a73a614bf
nakag <nakag@users.osdn.me>
2019-09-29 15:49:19 +0900

    Remove unused method.

--- bundles/tmdmaker.ui.menu/src/jp/sourceforge/tmdmaker/ui/menu/Activator.java
+++ bundles/tmdmaker.ui.menu/src/jp/sourceforge/tmdmaker/ui/menu/Activator.java
@@ -15,7 +15,6 @@
  */
 package jp.sourceforge.tmdmaker.ui.menu;
 
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -65,15 +64,4 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
-	/**
-	 * Returns an image descriptor for the image file at the given plug-in
-	 * relative path
-	 * 
-	 * @param path
-	 *            the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
-	}
 }
