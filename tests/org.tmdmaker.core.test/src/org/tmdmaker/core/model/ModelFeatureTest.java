35cf878079e571482eaa2b57b942540dfddd8164
nakag <nakag@users.osdn.me>
2019-12-10 08:17:18 +0900

    Update copyright.

--- tests/org.tmdmaker.core.test/src/org/tmdmaker/core/model/ModelFeatureTest.java
+++ tests/org.tmdmaker.core.test/src/org/tmdmaker/core/model/ModelFeatureTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2015 TMD-Maker Project <http://tmdmaker.sourceforge.jp/>
+ * Copyright 2009-2019 TMD-Maker Project <https://www.tmdmaker.org>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
