99c2114eb327e67527596257d26312cba45bb79b
nakag <nakag@users.osdn.me>
2023-08-12 17:16:34 +0900

    Add support 2023-06.

--- tests/org.tmdmaker.ui.test/src/org/tmdmaker/ui/editors/MoveTester.java
+++ tests/org.tmdmaker.ui.test/src/org/tmdmaker/ui/editors/MoveTester.java
@@ -15,12 +15,15 @@
  */
 package org.tmdmaker.ui.editors;
 
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swtbot.eclipse.gef.finder.SWTGefBot;
 import org.eclipse.swtbot.eclipse.gef.finder.widgets.SWTBotGefEditor;
 import org.eclipse.swtbot.swt.finder.keyboard.Keyboard;
 import org.eclipse.swtbot.swt.finder.keyboard.KeyboardFactory;
 import org.eclipse.swtbot.swt.finder.keyboard.Keystrokes;
 import org.eclipse.swtbot.swt.finder.utils.SWTBotPreferences;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.tmdmaker.ui.test.AbstractTester;
 
 /**
@@ -30,6 +33,7 @@ import org.tmdmaker.ui.test.AbstractTester;
  *
  */
 public class MoveTester extends AbstractTester {
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	public MoveTester(SWTGefBot bot, SWTBotGefEditor botEditor) {
 		super(bot, botEditor);
@@ -37,25 +41,48 @@ public class MoveTester extends AbstractTester {
 
 	@Override
 	protected void doTest() {
+
+		botEditor.setFocus();
+		logger.info("Activate Entity tool");
+		botEditor.activateTool("Entity");
+
+		logger.info("Activate Select tool");
 		botEditor.activateTool("Select");
 
+		logger.info("Select tool click");
 		botEditor.click(55, 55);
 
+		logger.info("Select tool drag");
 		botEditor.drag(55, 55, 100, 100);
 		wait.waitDefault();
 		botEditor.getEditPart("顧客").select();
 		botEditor.activateTool("Select");
 
+		logger.info("keyboard");
 		SWTBotPreferences.KEYBOARD_LAYOUT = "EN_US";
-		Keyboard keyboard = KeyboardFactory.getSWTKeyboard();
-		keyboard.pressShortcut(Keystrokes.LEFT);
-		keyboard.pressShortcut(Keystrokes.LEFT);
-		keyboard.pressShortcut(Keystrokes.DOWN);
-		keyboard.pressShortcut(Keystrokes.DOWN);
-		keyboard.pressShortcut(Keystrokes.RIGHT);
-		keyboard.pressShortcut(Keystrokes.RIGHT);
-		keyboard.pressShortcut(Keystrokes.UP);
-		keyboard.pressShortcut(Keystrokes.UP);
+		Display.getDefault().syncExec(new Runnable() {
+			public void run() {
+				try {
+					Keyboard keyboard = KeyboardFactory.getSWTKeyboard();
+
+					logger.info("keyboard pressShortcut left");
+					keyboard.pressShortcut(Keystrokes.LEFT);
+					keyboard.pressShortcut(Keystrokes.LEFT);
+					logger.info("keyboard pressShortcut down");
+					keyboard.pressShortcut(Keystrokes.DOWN);
+					keyboard.pressShortcut(Keystrokes.DOWN);
+					logger.info("keyboard pressShortcut right");
+					keyboard.pressShortcut(Keystrokes.RIGHT);
+					keyboard.pressShortcut(Keystrokes.RIGHT);
+					logger.info("keyboard pressShortcut up");
+					keyboard.pressShortcut(Keystrokes.UP);
+					keyboard.pressShortcut(Keystrokes.UP);
+
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 	}
 
 }
